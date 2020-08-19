cffcccfda966077c3f698eaa3a57f9e17b109211
Fabien Griselles <fgriselles@hotmail.fr>
2020-08-19 16:01:25 +0200

    Add our first route
    - handle /api/stuff route
    - handle cors errors

--- app.js
+++ app.js
@@ -1,29 +1,35 @@
 const express = require('express');
-
 const app = express();
 
-
-
-// la fonction utilisée par use est un appelé middleware dans express
+// Prevent CORS errors on all request
 app.use((req, res, next) => {
-    console.log("j'ai reçu une requête");
-    next(); // Nécessaire pour passer au middleware suivant
-});
-
-app.use((req, res, next) => {
-    res.status(200);
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
 });
 
-app.use((req, res, next) => {
-    res.json({
-        message: "Coucou le monde"
-    });
-    next();
-});
-
-app.use((req, res) => {
-    console.log("La requête s'est bien passée");
-});
+// Handle request to "/api/stuff"
+app.use('/api/stuff', (req, res, next) => {
+    const stuff = [
+      {
+        _id: 'oeihfzeoi',
+        title: 'Mon premier objet',
+        description: 'Les infos de mon premier objet',
+        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
+        price: 4900,
+        userId: 'qsomihvqios',
+      },
+      {
+        _id: 'oeihfzeomoihi',
+        title: 'Mon deuxième objet',
+        description: 'Les infos de mon deuxième objet',
+        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
+        price: 2900,
+        userId: 'qsomihvqios',
+      },
+    ];
+    res.status(200).json(stuff);
+  });
 
 module.exports = app;
