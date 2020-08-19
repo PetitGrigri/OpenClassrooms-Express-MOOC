8520b594b56ff73d171df9c0518d778245d50e16
Fabien Griselles <fgriselles@hotmail.fr>
2020-08-19 17:31:21 +0200

    Add mongoose + connect to mongodb

--- README.md
+++ README.md
@@ -2,16 +2,22 @@
 A simple repository which is created to fallow the [MOOC about express on OpenClassrooms](https://openclassrooms.com/fr/courses/6390246-passez-au-full-stack-avec-node-js-express-et-mongodb)
 
 ## Used commands
-- node server : lauch the server on port 3000
-- nodemon server : lauch the server on port 3000 but the server is update according the sources.
+- Frontend :
+    - ng serve
+- Backend
+    - node server : lauch the server on port 3000
+    - nodemon server : lauch the server on port 3000 but the server is update according the sources.
 
 ## Tags
 - 1.3 : Simple node server
 - 1.4 : Simple express server
 - 1.5 : Create our first route (all methods accepted)
 - 1.6 : Handdle Post and Get
+- 2.1 : Add MongoDB, Mongo-express (with docker-compose) and mongoose
 
 ## Dependencies
+- angular cli (npm install -g @angular/cli)
 - node (12.18.1) & npm (6.14.5)
 - nodemon (npm install -g nodemon)
 - body-parser (npm install --save body-parser)
+- mongoose (npm install --save mongoose)
--- app.js
+++ app.js
@@ -1,22 +1,30 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://root:example@localhost:27017',{ 
+  useNewUrlParser: true,
+  useUnifiedTopology: true 
+})
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 const app = express();
 
 // Prevent CORS errors on all request
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-    next();
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
 });
 
 app.use(bodyParser.json());
 
 // Handle GET request to "/api/stuff"
 app.post('/api/stuff', (req, res, next) => {
-    console.log(req.body);
-    res.status(201).end()
+  console.log(req.body);
+  res.status(201).end()
 });
 
 // Handle GET request to "/api/stuff"
